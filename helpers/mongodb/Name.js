393e9b8b2b85327a933806fd14249f900c52c810
Ohav <ohav99053@gmail.com>
2024-11-11 12:04:38 +0200

    fixed middlename problem

--- helpers/mongodb/Name.js
+++ helpers/mongodb/Name.js
@@ -3,16 +3,7 @@ const { DEFAULT_VALIDATION } = require("./mongooseValidators");
 
 const Name = new mongoose.Schema({
   first: DEFAULT_VALIDATION,
-  middle: {
-    ...DEFAULT_VALIDATION,
-    required: false,
-    validate: {
-      validator: function (v) {
-        return v === undefined || v.length >= 2;
-      },
-      message: props => `${props.value} is shorter than the minimum allowed length (2).`,
-    },
-  },
+  middle: {},
   last: DEFAULT_VALIDATION,
   _id: false
 });
