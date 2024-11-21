5b59b5cff4b93c30a33fa79973e3c3b341e8b318
OhavShemesh <101662957+OhavShemesh@users.noreply.github.com>
2024-11-21 22:42:54 +0200

    Update cors.js

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -2,16 +2,7 @@ const cors = require("cors");
 
 const corsMiddleware = cors({
   origin: [
-    "http://127.0.0.1:5500",
-    "http://localhost:5500",
-    "http://localhost:3000",
-    "http://localhost:5173",
-    "http://localhost:5174",
-    "http://192.168.1.148:5173",
-    "https://cardsproject-58xb.onrender.com",
-    "http://10.244.128.148:5173",
-    "https://newfinalproject-j2rp.onrender.com/"
-
+    "*"
   ],
 });
 
